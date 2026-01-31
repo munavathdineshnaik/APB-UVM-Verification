d94d23d89807c1fc4d16a0e28be0184ab282dedc
munavathdineshnaik <munavathdineshnaik999@gmail.com>
2026-01-31 17:30:48 +0530

    Update apb_env.sv

--- dv/env/apb_env.sv
+++ dv/env/apb_env.sv
@@ -1,1 +1,21 @@
-apbenv
+class apb_env extends uvm_env;
+  `uvm_component_utils(apb_env)
+
+  apb_agent      agent;
+  apb_scoreboard scoreboard;
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    agent = apb_agent::type_id::create("agent", this);
+    scoreboard = apb_scoreboard::type_id::create("scoreboard", this);
+  endfunction
+
+  virtual function void connect_phase(uvm_phase phase);
+    // Connect Monitor to Scoreboard
+    agent.monitor.item_collected_port.connect(scoreboard.item_collected_export);
+  endfunction
+endclass
