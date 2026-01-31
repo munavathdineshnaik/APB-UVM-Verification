4a56a6420d62efc9fad76931c918ab90bda8f74f
munavathdineshnaik <munavathdineshnaik999@gmail.com>
2026-01-31 17:32:34 +0530

    Update apb_base_seq.sv

--- dv/seq/apb_base_seq.sv
+++ dv/seq/apb_base_seq.sv
@@ -1,1 +1,20 @@
-baseseq
+class apb_base_seq extends uvm_sequence #(apb_transaction);
+  `uvm_object_utils(apb_base_seq)
+
+  function new(string name = "apb_base_seq");
+    super.new(name);
+  endfunction
+
+  virtual task body();
+    // Generate 10 random transactions
+    repeat(10) begin
+      req = apb_transaction::type_id::create("req");
+      start_item(req);
+      
+      // Randomize address and data
+      if (!req.randomize()) `uvm_error("SEQ", "Randomization failed")
+      
+      finish_item(req);
+    end
+  endtask
+endclass
